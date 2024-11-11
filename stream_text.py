7e37f0d2c27aaacd29c0cbcbc8a24722bb95fb91
Unim Agiande <euagiande@gmail.com>
2024-11-11 15:26:07 +0100

    Add files via upload

---
+++ stream_text.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+st.title("Streamlit Basics")
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.write("This is regular text using `st.write()`")
+st.markdown("**This text is bold in markdown**")
+
