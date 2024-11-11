fcf463bad0791a21b9368e07c5444160705763ff
Unim Agiande <euagiande@gmail.com>
2024-11-11 15:25:10 +0100

    Add files via upload

---
+++ stream1 (1).py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+st.title("Hello, Stream, I'm lit!")
+st.write("Welcome to my Streamlit app.")
+st.write("You can STREAM here without NETFLIX.")
+st.write("And you can be LIT without a LIGHTER.")
+
